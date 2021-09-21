5ac1cd7251e087ab8c3bb46efcc8d4024bd0af24
James Esh <jamesesh@live.com>
2021-09-21 09:52:02 +0000

    Merged PR 19: Add uniquee tracking
    Add uniquee tracking
    
    Related work items: #22

--- DevCon/DevCon.Tests/TrackerTests.cs
+++ DevCon/DevCon.Tests/TrackerTests.cs
@@ -11,9 +11,10 @@ namespace DevConClicker.Tests
             var tracker = new Tracker();
             Assert.Equal(0, tracker.TotalVisits);
 
-            tracker.Track();
-            tracker.Track();
-            tracker.Track();
+            tracker.Track("test");
+            tracker.Track("test");
+            tracker.Track("test2");
+            Assert.Equal(2, tracker.UniqueVisits);
             Assert.Equal(3, tracker.TotalVisits);
         }
     }
--- DevCon/DevCon/Tracker.cs
+++ DevCon/DevCon/Tracker.cs
@@ -1,12 +1,20 @@
-﻿namespace DevCon
+﻿using System.Collections.Concurrent;
+using System.Linq;
+
+namespace DevCon
 {
     public class Tracker
     {
         public int TotalVisits { get; private set; }
+        public int UniqueVisits => IPVisits.Count;
 
-        public void Track()
+        static ConcurrentBag<string> IPVisits { get; } = new ConcurrentBag<string>();
+
+        public void Track(string ip)
         {
             TotalVisits++;
+            if (!IPVisits.Contains(ip))
+                IPVisits.Add(ip);
         }
     }
 }
