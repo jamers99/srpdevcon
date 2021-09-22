549fca5c894f28801b18556750367a31974bc9fd
James Esh <jamesesh@live.com>
2021-09-22 08:05:59 +0000

    Merged PR 21: Unique visits
    Related work items: #22

--- DevCon/DevCon/Tracker.cs
+++ DevCon/DevCon/Tracker.cs
@@ -6,10 +6,15 @@ namespace DevCon
     public class Tracker
     {
         public int TotalVisits { get; set; }
+        public int UniqueVisits => IPVisits.Count;
 
-        public void Track()
+        public ConcurrentBag<string> IPVisits { get; } = new ConcurrentBag<string>();
+
+        public void Track(string ip)
         {
             TotalVisits++;
+            if (!IPVisits.Contains(ip))
+                IPVisits.Add(ip);
         }
     }
 }
