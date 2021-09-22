0fc3e2bc2669ceaeeb10298c8ed0533718f44940
James Esh <jamesesh@live.com>
2021-09-22 08:27:46 +0000

    Merged PR 25: Unique visits
    Related work items: #22

--- DevCon/DevCon.Tests/TrackerTests.cs
+++ DevCon/DevCon.Tests/TrackerTests.cs
@@ -10,11 +10,13 @@ namespace DevConClicker.Tests
         {
             var tracker = new Tracker();
             Assert.Equal(0, tracker.TotalVisits);
+            Assert.Equal(0, tracker.UniqueVisits);
 
-            tracker.Track();
-            tracker.Track();
-            tracker.Track();
+            tracker.Track("173.194.222.113");
+            tracker.Track("64.233.165.190");
+            tracker.Track("64.233.165.190");
             Assert.Equal(3, tracker.TotalVisits);
+            Assert.Equal(2, tracker.UniqueVisits);
         }
     }
 }
