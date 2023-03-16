b96f4ce7a7df1deb02ed4cea5e554fe1fe031d51
James Esh <jamesesh@live.com>
2023-03-16 16:03:36 +0500

    make a problem

---
+++ DevCon2023/Pages/Coffee.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevCon2023.Pages
+{
+    public class CoffeeModel : PageModel
+    {
+        public void OnGet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
